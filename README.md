9f2c8b4e093efd8af400927c0fdd38b27f0e65cf
Chris Russell <cruse1977123@gmail.com>
2020-10-22 09:51:54 +0000

    added descriptions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MAC-HUNTER
 Script to locate mac-addresses, based on the original .@IPVZERO code, this uses show mac address-table eather than show interfaces
+
+updated to show descriptions
--- mac-hunter.py
+++ mac-hunter.py
@@ -1,7 +1,13 @@
 """
 
 AUTHOR: IPvZero(main), kit_chrisr(modifications)
-DATE: 21 Oct 2020
+DATE: 22 Oct 2020
+
+changelog:
+2020/10/21 - ipvzero: first code (majority)
+2020/10/21 - kit_chrisr: change to show mac address-table
+2020/10/22 - kit_chrisr: include descriptions on interfaces
+
 
 """
 
@@ -10,7 +16,7 @@ import logging
 from nornir import InitNornir
 from nornir_netmiko.tasks import netmiko_send_command
 from rich import print as rprint
-
+import json
 nr = InitNornir(config_file="config.yaml")
 break_list = []
 
@@ -21,10 +27,11 @@ target = input("Enter the mac address you wish to find: ")
 
 def pull_info(task):
 
+
     """
     1) If MAC address is present, identify Device and Interface
     """
-
+    
     result = task.run(
         task=netmiko_send_command, command_string="show mac address-table", use_genie=True
     )
@@ -35,9 +42,26 @@ def pull_info(task):
             for mac_address in mac_addresses[vlan]["mac_addresses"].keys():
                 if target == mac_address:
                     break_list.append(target)
-                    print_info(task,mac_address, vlan, mac_addresses[vlan]["mac_addresses"][mac_address]["interfaces"])
+                    interface_descr = get_interface_descriptions(task,mac_addresses[vlan]["mac_addresses"][mac_address]["interfaces"])
+                    print_info(task,mac_address, vlan, mac_addresses[vlan]["mac_addresses"][mac_address]["interfaces"],interface_descr)
 
-def print_info(task, mac_address,vlan,interfaces):
+
+def get_interface_descriptions(task, interfaces):
+    descriptions = {}
+    iresult = task.run(
+        task=netmiko_send_command, command_string="show interfaces", use_genie=True
+    )
+
+    # note show interface {interface} would be nicer, but genie has an issue doing this on port-channels
+    for interface in interfaces.keys():
+        if interface in iresult.result:
+            descriptions[interface] = iresult.result[interface]['description']
+        else:
+            descriptions[interface] = 'n/a'
+    return descriptions
+
+
+def print_info(task, mac_address,vlan,interfaces,descriptions):
 
     """
     Execute show cdp neighbor and show version commands
@@ -45,7 +69,7 @@ def print_info(task, mac_address,vlan,interfaces):
     """
     str_interface = ""
     for interface in interfaces.keys():
-        str_interface = str_interface + interface + ","
+        str_interface = str_interface + interface + "(" + descriptions[interface] + "),"
     rprint("\n[green]*** TARGET IDENTIFIED ***[/green]")
     print(f"MAC ADDRESS: {target} is present on {task.host}'s")
     rprint("\n[cyan]GENERATING DETAILS...[/cyan]")
